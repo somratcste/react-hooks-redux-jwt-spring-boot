5d8e4361a82445af96d8921903b881ca8f835199
somratcste <somratcste@gmail.com>
2021-05-11 23:45:06 +0200

    task: create quote service

---
+++ frontend/src/services/QuoteService.js
@@ -0,0 +1,41 @@
+import http from '../http-common';
+
+const getAll = () => {
+    return http.get("/quotes");
+};
+
+const get = id => {
+    return http.get(`/quotes/${id}`);
+};
+
+const create = data => {
+    return http.post("/quotes", data);
+};
+
+const update = (id, data) => {
+    return http.put(`/quotes/${id}`, data);
+};
+
+const remove = id => {
+    return http.delete(`/quotes/${id}`);
+};
+
+const removeAll = () => {
+    return http.delete(`/quotes`);
+};
+
+const findByTitle = title => {
+    return http.get(`/quotes?title=${title}`);
+};
+
+const QuoteService = {
+    getAll,
+    get,
+    create,
+    update,
+    remove,
+    removeAll,
+    findByTitle
+};
+
+export default QuoteService;
